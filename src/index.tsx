a260479ac4db35c9746a4f344d17b59503b7f558
dumbus <dumbusx@gmail.com>
2025-05-31 19:00:07 +0300

    init: init frontend-virtualization-performance

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import App from 'app/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLDivElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
