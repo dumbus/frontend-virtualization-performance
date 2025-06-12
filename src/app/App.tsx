497d30bc00995b521af02d3aa93be833f55b1a48
dumbus <dumbusx@gmail.com>
2025-06-12 13:38:32 +0300

    feat: add react-router-dom and url routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { ReactVirtualizedPage } from 'pages';
 import { ReactWindowPage } from 'pages';
 
 import 'styles/styles.scss';
 
 const App = () => {
-  return <ReactVirtualizedPage visibleColumnCount={10} visibleRowCount={10} />;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/react-virtualized"
+          element={<ReactVirtualizedPage visibleColumnCount={10} visibleRowCount={10} />}
+        />
+        <Route path="/react-window" element={<ReactWindowPage visibleColumnCount={10} visibleRowCount={10} />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
